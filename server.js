b28fcec92db617e0728988d934dba2542b8536b5
ince4 <421603832@qq.com>
2020-04-17 22:09:57 +0800

    character-parser

--- server.js
+++ server.js
@@ -1,23 +1,52 @@
-// const https = require('https')
+const http = require('http')
 const fs = require('fs')
 const cheerio = require('cheerio')
-const querystring = require('querystring')
 const request = require('request')
-// const express = require('express')
-
-// const app = express()
-// app.listen(3333, () => {
-//     console.log('listening at port 3333')
-// })
 
 let URL = 'https://ultimateframedata.com/'
-
-request({
+let reqOptions = {
     url: URL,
     method: 'GET',
     headers: {
         'content-type': 'text/html'
     }
-}, function (error, response, body) {
-       console.log(body)
-})
+}
+
+function getNodeList (selector) {
+    return new Promise ((resolve, reject) => {
+
+        request(reqOptions, function(error, response, body) {
+            if (error != null) {
+                reject(error)
+            } else {
+                let $ = cheerio.load(body)
+                let nodeList = Object.values($(selector))
+                resolve(nodeList)
+            }
+        })
+
+    })
+}
+
+// 返回角色列表
+async function getCharList() {
+    try {
+        console.log('waiting response')
+        let nodeList = await getNodeList('#charList .charactericon a')
+        console.log('get response data')
+        let charList = nodeList.slice(1)
+        
+        let charData = {}
+        for (let i = 0; i < charList.length - 4; i++) {
+            charData[charList[i]['attribs']['title']] = {
+                name: charList[i]['attribs']['title'],
+                href: charList[i]['attribs']['href']
+            }
+        }
+        return charData
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+getCharList().then(data => {console.log(data, '123')})
